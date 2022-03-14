f51de68f7720941810772b1f44f74481315f40ce
Umut Ulupinar <bjk.umut.97@gmail.com>
2022-03-14 17:43:01 +0300

    newviews

---
+++ BusinessLayer/Concrete/CategoryManager.cs
@@ -0,0 +1,42 @@
+﻿using DataAccessLayer.Concrete.Repositories;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class CategoryManager
+    {
+        GenericRepository<Category> repo = new GenericRepository<Category>();
+
+        public List<Category> GetAllBl()
+        {
+            return repo.List();
+        }
+
+        public void InsertCategoryBl(Category p)
+        {
+            if (p.CategoryName == "" || p.CategoryName.Length < 3 || p.CategoryDescription == ""
+                || p.CategoryDescription.Length > 50)
+            {
+                //hata mesajı
+            }
+            else
+            {
+                repo.Insert(p);
+            }
+        }
+
+        //public void DeleteCategoryBl(Category p) 
+        //{
+        //    repo.Delete(p); 
+        //}
+
+
+
+
+    }
+}
