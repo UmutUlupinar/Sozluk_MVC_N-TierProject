79acbc2f045ad8500de483a52a34430ed4a2d802
Umut Ulupinar <bjk.umut.97@gmail.com>
2022-03-17 19:13:18 +0300

    writerValidIndexAddingIsDone

---
+++ BusinessLayer/ValidationRules/WriterValidator.cs
@@ -0,0 +1,28 @@
+﻿using EntityLayer.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.ValidationRules
+{
+    public class WriterValidator : AbstractValidator<Writer>
+    {
+        public WriterValidator()
+        {
+            RuleFor(a => a.WriterName).NotEmpty().WithMessage("Kategori Adını doldurunuz");
+            RuleFor(a => a.WriterSurname).NotEmpty().WithMessage("Açıklama giriniz");
+            RuleFor(a => a.WriterAbout).NotEmpty().WithMessage("Hakkında bilgisi giriniz");
+            RuleFor(a => a.WriterMail).NotEmpty().WithMessage("Mail adresi giriniz");
+            RuleFor(a => a.WriterName).MinimumLength(3).WithMessage("İsim en az 3 en fazla 20 karakter olmalı");
+            RuleFor(a => a.WriterName).MaximumLength(20).WithMessage("İsim en az 3 en fazla 20 karakter olmalı");
+            RuleFor(a => a.WriterMail).MinimumLength(3).WithMessage("İsim en az 3 en fazla 20 karakter olmalı");
+            RuleFor(a => a.WriterMail).MaximumLength(20).WithMessage("Mail en az 3 en fazla 20 karakter olmalı");
+            RuleFor(a => a.WriterSurname).MinimumLength(3).WithMessage("Soyisim en az 3 en fazla 20 karakter olmalı");
+            RuleFor(a => a.WriterSurname).MaximumLength(50).WithMessage("Soyisim en az 3 en fazla 50 karakter olmalı");
+        }
+
+    }
+}
---
+++ Sozluk_MVC_N-TierProject/Controllers/WriterController.cs
@@ -0,0 +1,51 @@
+﻿using BusinessLayer.Concrete;
+using BusinessLayer.ValidationRules;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using FluentValidation.Results;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Sozluk_MVC_N_TierProject.Controllers
+{
+    public class WriterController : Controller
+    {
+        // GET: Writer
+
+        WriterManager wm = new WriterManager(new EfWriterDal());
+        public ActionResult Index()
+        {
+            var WriterValues = wm.GetList();
+            return View(WriterValues);
+        }
+
+        [HttpGet]
+        public ActionResult AddWriter()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddWriter(Writer writer) 
+        {
+            WriterValidator wv = new WriterValidator();
+            ValidationResult results = wv.Validate(writer);
+            if (results.IsValid)
+            {
+                wm.WriterAdd(writer);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                foreach(var item in results.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+            }
+            return View();
+        }
+    }
+}
